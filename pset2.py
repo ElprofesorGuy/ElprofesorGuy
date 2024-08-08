ef303766115715395d30ee8f7868aa849ea17507
ElprofesorGuy <guyeinstein15@gmail.com>
2024-08-08 15:44:39 +0100

    initialisation de Hangman

--- pset2.py
+++ pset2.py
@@ -1,2 +1,10 @@
-print("Hello world!!!")
-print("Is very too strong")
+"""print("Hello world!!!")
+print("Is very too strong")"""
+
+s="6.00 is 6.0001 and 6.002"
+string=""
+string += s[-1]
+string += s[0]
+string += s[4::30]
+string += s[13:10:-1]
+print(string)
